eaf5d6cd26d3d3d30dcfef2dea1fd4ce8a1975bf
Ujjwal Poudel <upoudel1@my.centennialcollege.ca>
2023-12-05 18:57:30 -0500

    Initiated home page and navigation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Login from "./component/login/login";
-import Signup from "./component/signUp/signup";
+import Navigation from "./component/navbar/navigation";
+// import Login from "./component/login/login";
+// import Signup from "./component/signUp/signup";
 
 function App() {
   return (
     <div className="App">
       <h1>Front Page</h1>
-      <Login/>
-      <Signup/>
+      {/* <Login/>
+      <Signup/> */}
+
+      <Navigation />
     </div>
   );
 }
---
+++ frontend/src/component/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <div>
+        <h1 id="total"></h1>
+      </div>
+      
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
