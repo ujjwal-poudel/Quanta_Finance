37ece194ad72e8848471f9f3f7d169177757fa01
ujjwal <301284284@cencol.ca>
2023-12-05 20:06:25 -0500

    navigation bar > added nav links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Navigation from "./component/navbar/navigation";
-// import Login from "./component/login/login";
-// import Signup from "./component/signUp/signup";
+// import Navigation from "./component/navbar/navigation";
+import Login from "./component/login/login";
+import Signup from "./component/signUp/signup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Front Page</h1>
-      {/* <Login/>
-      <Signup/> */}
+      <Login/>
+      <Signup/>
 
-      <Navigation />
+      {/* <Navigation /> */}
     </div>
   );
 }
--- frontend/src/component/navbar/navigation.js
+++ frontend/src/component/navbar/navigation.js
@@ -1,30 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./navigation.css";
 
 const Navigation = () => {
 
+  const initialExpense = 20.37;
+  const initializedExpense = initialExpense.toFixed(2);
+
   return (
-    <div>
+    <div className="navigation">
       <div className="expense-container">
-        <h1 id="totalExpense"></h1>
+        <h1>$<span id="totalExpense">{initializedExpense}</span></h1>
       </div>
 
-      <div className="navigation">
+      <ul className="navigation-navbar">
         {/* Expenses */}
-        <div>
-          <h2>Expenses</h2>
-        </div>
+        <li className="navigation-title">
+          <NavLink to="/expenses">
+            Expenses
+          </NavLink>
+        </li>
 
         {/* Categories */}
-        <div>
+        <li className="navigation-title">
           <h2>Categories</h2>
-        </div>
+        </li>
 
         {/* Charts */}
-        <div>
+        <li className="navigation-title">
           <h2>Charts</h2>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 };
